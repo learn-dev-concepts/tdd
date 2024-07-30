6e18d1aa64f6aa6c804255ffe62a8ee910ea4785
devsince2021 <dev.ryu2021@gmail.com>
2024-07-30 16:01:49 +0900

    61: 테스트 통과를 위한 가짜로 구현하기

--- src/test/java/com/tdd/_1/currency/Dollar.java
+++ src/test/java/com/tdd/_1/currency/Dollar.java
@@ -10,4 +10,10 @@ public class Dollar {
     public Dollar times(int multiplier) {
         return new Dollar(amount * multiplier);
     }
+
+
+    @Override
+    public boolean equals(Object object) {
+        return true;
+    }
 }
