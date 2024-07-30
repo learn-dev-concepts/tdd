2412bb4ffc400e0e43d07ed0dae048f59d2cd6dc
devsince2021 <dev.ryu2021@gmail.com>
2024-07-30 17:58:48 +0900

    71: 연습해볼 순서 작성

---
+++ src/test/java/com/tdd/_1/currency/FrancTest.java
@@ -0,0 +1,21 @@
+package com.tdd._1.currency;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+// 테스트 작성하기
+// 컴파일 되게 하기
+// 실패하는지 확인하기
+// 실행되도록 만들기
+// 중복 제거
+
+public class FrancTest {
+
+    @Test
+    public void testFrancMultiplication() {
+        Franc five = new Franc(5);
+        assertEquals(new Franc(10), five.times(2));
+        assertEquals(new Franc(15), five.times(3));
+    }
+}
