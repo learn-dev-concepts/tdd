eaf8de3948838d7b458b747d4e1f061c92976880
devsince2021 <dev.ryu2021@gmail.com>
2024-07-31 15:49:08 +0900

    chore: 테스트 파일 1개로 병합

---
+++ src/test/java/com/tdd/_1/currency/TestDriver.java
@@ -0,0 +1,33 @@
+package com.tdd._1.currency;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TestDriver {
+
+    @Test
+    public void testDollarMultiplication() {
+        Money five = Money.dollar(5);
+        assertEquals(Money.dollar(10), five.times(2));
+        assertEquals(Money.dollar(15), five.times(3));
+    }
+
+    @Test
+    public void testFrancMultiplication() {
+        Franc five = Money.franc(5);
+        assertEquals(Money.franc(10), five.times(2));
+        assertEquals(Money.franc(15), five.times(3));
+    }
+
+
+    @Test
+    public void testEquality() {
+        assertTrue(Money.dollar(5).equals(Money.dollar((5))));
+        assertFalse(Money.dollar(5).equals(Money.dollar(6)));
+        assertTrue(Money.franc(5).equals(Money.franc(5)));
+        assertFalse(Money.franc(5).equals(Money.franc(10)));
+        assertFalse(Money.dollar(5).equals(Money.franc(5)));
+
+    }
+}
