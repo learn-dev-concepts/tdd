88dd84cd14673d5cb1de7a2eb531412beee35dc8
devsince2021 <dev.ryu2021@gmail.com>
2024-08-06 10:35:44 +0900

    144: 플러스 구현

--- src/test/java/com/tdd/_1/currency/TestDriver.java
+++ src/test/java/com/tdd/_1/currency/TestDriver.java
@@ -5,6 +5,16 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class TestDriver {
+    @Test
+    public void testSumPlusMoney() {
+        Expression fiveBucks = Money.dollar(5);
+        Expression tenFrancs = Money.franc(10);
+        Bank bank = new Bank();
+        bank.addRate("CHF", "USD", 2);
+        Expression sum = new Sum(fiveBucks, tenFrancs).plus(fiveBucks);
+        Money result = bank.reduce(sum, "USD");
+        assertEquals(Money.dollar(15), result);
+    }
 
     @Test
     public void testMixedAddition() {
