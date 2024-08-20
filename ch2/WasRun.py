d9291a7ac9626b0985bd92a5dcffab72edad219c
devsince2021 <dev.ryu2021@gmail.com>
2024-08-20 13:28:09 +0900

    186: testBrokenMethod 작성

--- ch2/WasRun.py
+++ ch2/WasRun.py
@@ -10,23 +10,37 @@ class TestCase:
         pass
 
     def run(self):
+        result = TestResult()
+        result.testStarted()
+
         self.setUp()
         method = getattr(self, self.name)
         method()
         self.tearDown()
+        return result
 
+
+class TestResult:
+    def __init__(self):
+        self.runCount = 0
+    
+    def testStarted(self):
+        self.runCount = self.runCount + 1
+    
+    def summary(self):
+        return "%d run, 0 failed" % self.runCount
 
 
 class WasRun(TestCase):
 
     def testMethod(self):
-        self.wasRun = 1
         self.log = self.log + "testMethod "
 
     def setup(self):
-        self.wasRun = None
-        self.wasSetUp = 1
         self.log = "setUp "
     
     def tearDown(self):
         self.log = self.log + "tearDown "
+
+    def testBrokenMethod(self):
+        raise Exception
--- ch2/main.py
+++ ch2/main.py
@@ -3,6 +3,16 @@ from WasRun import TestCase, WasRun
 
 
 class TestCaseTest(TestCase):
+
+    def testFailedResult(self):
+        test = WasRun("testBrokenMethod")
+        result = test.run()
+        assert("1 run, 1 failed" == result.summary())
+
+    def testResult(self):
+        test = WasRun("testMethod")
+        result = test.run()
+        assert("1 run, 0 failed" == result.summary())
    
     def testTemplateMethod(self):
         test = WasRun("testMethod")
